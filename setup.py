06b8b1dad19ce543f2b3c39c46ceac24e9882912
Barry John Williams <barry.williams@linn.co.uk>
2017-05-28 10:03:06 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'tapsaff',
+  packages = ['tapsaff'],
+  version = '0.0.1',
+  description = 'Provides an API for requesting information from taps-aff.co.uk',
+  author = 'Barry John Williams',
+  author_email = 'barry@bjw.me.uk',
+  url = 'https://github.com/bazwilliams/python-taps-aff',
+  download_url = 'https://github.com/bazwilliams/python-taps-aff/tarball/0.0.1',
+  keywords = ['weather', 'glasgow', 'clothes', 'top', 'taps', 'aff', 'oan'],
+  install_requires = ['requests'],
+  classifiers = [],
+)
---
+++ tapsaff/__init__.py
@@ -0,0 +1,29 @@
+"""
+Represent the status of aps-aff for a specified location
+"""
+
+import requests
+
+class TapsAff(object):
+    """tapsaff object with location"""
+
+    def __init__(self, location):
+        """Initialize with a location such as a UK postcode or city"""
+
+        self.location = location
+
+    def is_taps_aff(self):
+        """Returns True if taps aff for this location"""
+
+        request = requests.get('http://www.taps-aff.co.uk/?location=%s?api' % self.location)
+        if request.status_code == 200:
+            taps = request.json()['taps']
+            if taps == 'aff':
+                return True
+            elif taps == 'oan':
+                return False
+            else:
+                raise RuntimeError("Unexpected taps value: %s" % taps)
+        else:
+            print(request.text)
+            raise IOError("Failure downloading from Api")
